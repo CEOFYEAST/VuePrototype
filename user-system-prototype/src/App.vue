4dd6b7a0b79df23118c3d67d2df528062c3881f7
CEOFYEAST <benton.diebold@me.com>
2024-08-18 22:06:10 -0700

    Started moving old progress into a vue project

---
+++ user-system-prototype/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <SignInForm  />
+</template>
+
+<script>
+  import SignInForm from './components/SignInForm.vue'
+
+  components: {
+    SignInForm
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ user-system-prototype/src/components/SignInForm.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="header-bar" align="right">
+
+    <button id="close-button" class="x1">X</button>
+    
+    </div>
+
+    <form id="sign-in-form" class="flex column">
+
+        <input type="email" placeholder="Email" id="user-email" name="userEmail" required><br>
+        <input type="password" placeholder="Password" id="user-password" name="userPassword" required><br>
+
+        <div id="submit-container" class="flex">
+            <div class="x3"></div>
+            <button id="submit" type="submit" class="x2">Submit</button>
+            <div class="x3"></div>
+        </div>
+
+        <div id="create-account-pointer-container">
+            <p id="create-account-pointer-link">Don't have an account? Create one.</p>
+            <p id="create-account-pointer">Creating an account allows you to store production vals. for up to three factories across sessions.</p>
+        </div>
+
+    </form>
+</template>
+
+<style scoped>
+    input {
+        font-size: 16px;
+        padding-left:5px;
+    }
+
+    #sign-in-dialog {
+        padding: 0px;
+        width: 400px;
+        position: absolute;
+        text-align: center;
+    }
+
+    #sign-in-form {
+        margin: 15px;
+        padding: 5px;
+    }
+
+    #close-button {
+        height: 25px;
+        width: 25px;
+    }
+
+    #submit {
+        height: 25px;
+    }
+
+    #create_account_pointer_container {
+        text-align: center;
+    }
+
+    #header-bar {
+        background-color: #c23838;
+        height: 25px;
+        width: 100%;
+    }
+</style>
