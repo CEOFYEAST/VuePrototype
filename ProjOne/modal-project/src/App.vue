bf0a951a28d1d4d2a46eee2729c84f7a039d84c5
CEOFYEAST <benton.diebold@me.com>
2024-08-18 19:23:21 -0700

    Created a new vue project for practice

--- ProjOne/modal-project/src/App.vue
+++ ProjOne/modal-project/src/App.vue
@@ -1,24 +1,14 @@
 <template>
-  <h1>{{ title }}</h1>
-  <input type="text" ref="name"> 
-  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      title: "My first vue app :)"
-    }
-  },
-  methods: {
-    handleClick() {
-      console.log( this.$refs.name )
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    }
+  components: {
+    Modal
   }
 }
 </script>
