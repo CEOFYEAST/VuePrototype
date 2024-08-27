74ee67e8551fe88ef8a2ece6a3a0d6abf2f6f7b6
CEOFYEAST <benton.diebold@me.com>
2024-08-26 19:35:37 -0700

    Added lifecycle hooks when reaction block is mounted and updated

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,15 +1,32 @@
 <template>
+  <div>
     <h1>Ninja Reaction Timer</h1>
-    <button @click="startGame()">Start</button>
+    <button @click="startGame()" :disabled="isPlaying">Start</button>
+    <Block :delay="delay" v-if="isPlaying"></Block>
+  </div>
 </template>
 
 <script>
+  import Block from './components/Block.vue'
+
   export default {
     name: 'app',
-    components: {}
-  }
-  methods {
-
+    components: {
+      Block
+    },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null
+      }
+    },
+    methods: {
+      startGame() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        console.log(this.delay)
+      }
+    }
   }
 </script>
 
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="block" v-if="showBlock">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        console.log('component mounted')
+        setTimeout(())
+    },
+    updated() {
+
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 400px; 
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
