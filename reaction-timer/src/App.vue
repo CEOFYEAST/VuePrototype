c1ae9d769f1467ac370a540d6557f36e5cec73c3
CEOFYEAST <benton.diebold@me.com>
2024-08-26 17:29:41 -0700

    Commiting changes before upgrading vue to latest version

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="startGame()">Start</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {}
   }
-}
+  methods {
+
+  }
 </script>
 
 <style>
